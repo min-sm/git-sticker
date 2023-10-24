34d022cf1abe9bd1520cea0f9eebb2462dfd89a9
DESKTOP-RCC3Q87\User <minsoemoe999@gmail.com>
2023-10-24 19:17:10 +0630

    msm fork test

--- keygen.js
+++
@@ -1,9 +0,0 @@
-const NodeRSA = require('node-rsa');
-const fs = require('fs');
-
-(async () => {
-  const key = new NodeRSA();
-  key.generateKeyPair();
-  fs.writeFileSync(`public.key`, key.exportKey('public'));
-  fs.writeFileSync(`private.key`, key.exportKey('private'));
-})();
